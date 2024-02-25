df380ff007adaf9e6aaa2a310f4a5517bb3be9cf
Diego <dl7designer@gmail.com>
2024-02-25 13:07:50 -0300

    Style do Modal Criado

---
+++ src/components/ModalDefaultStyle.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ModalDefaultStyled = styled.div`
+background-color: #fff;
+width: 480px;
+height: 480px;
+border: 1px solid inset;
+position: absolute;
+border-radius: 40px;
+box-shadow: 50px;
+top: 15%;
+padding: 40px;
+left: 32%;
+transform: translate(-50%, -50%),
+`
